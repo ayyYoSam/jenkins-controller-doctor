bed72ea7c4616a77d4ab71ced430208d180cb07a
ayyYoSam <voxxthisinnovember105@gmail.com>
2026-09-13 20:53:04 -0300

    Add Check, CheckResult, CheckStatus and others

---
+++ src/main/java/io/jenkins/controllerdoctor/checks/CheckStatus.java
@@ -0,0 +1,5 @@
+package main.java.io.jenkins.controllerdoctor.checks;
+
+public class CheckStatus {
+    
+}
---
+++ src/main/java/io/jenkins/controllerdoctor/client/JenkinsClient.java
@@ -0,0 +1,5 @@
+package main.java.io.jenkins.controllerdoctor.client;
+
+public class JenkinsClient {
+    
+}
